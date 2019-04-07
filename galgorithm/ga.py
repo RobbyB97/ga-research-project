7d68abbf7f2d861cc0e1acdb70059b68d03a1253
Robert Bergers <bergersr@my.easternct.edu>
2019-04-07 10:20:34 -0400

    rename genetic algorithm directory, initialize ga and make chromosome

---
+++ galgorithm/ga.py
@@ -0,0 +1,34 @@
+"""
+
+"""
+
+import logging
+import os
+from chessenv.board import Board
+from random import randint
+
+log = logging.getLogger('GA_Project')
+
+
+
+class GA:
+
+
+    def __init__(self, chromosome=None, size=8):
+
+        # Set attributes
+        self.size = size
+        self.env = Board(width=size, height=size)
+        if chromosome:
+            self.chromosome = chromosome
+        else:
+            self.make_chromosome()
+        return
+
+    def make_chromosome(self):
+
+        # Create chromosome for first gen GA
+        chromosome = []
+        for i in range(self.size):
+            gene = [i, randint(0, self.size-1)]
+            chromosome.append(gene)
