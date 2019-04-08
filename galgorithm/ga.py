1970023adff1f7e5118aa3927ea35868317e8289
Robert Bergers <bergersr@my.easternct.edu>
2019-04-08 12:48:19 -0400

    genetic algorithm creates and executes chromosomes

--- galgorithm/ga.py
+++ galgorithm/ga.py
@@ -18,18 +18,28 @@ class GA:
 
         # Set attributes
         self.size = size
+        self.complete = False
         self.env = Board(width=size, height=size)
         if chromosome:
             self.chromosome = chromosome
         else:
-            self.make_chromosome()
+            self.chromosome = self.make_chromosome()
         return
 
 
-    # Create chromosome for first gen GA
+    def try_chromosome(self):
+
+        for gene in self.chromosome:
+            self.env.place_queen(row=gene[0], column=gene[1])
+
+        self.env.print_board()
+
+
     def make_chromosome(self):
 
+        # Create chromosome for first gen GA
         chromosome = []
         for i in range(self.size):
             gene = [i, randint(0, self.size-1)]
             chromosome.append(gene)
+        return chromosome
