5b0059754a104856cc41b9801ec573210f91f118
Robert Bergers <bergersr@my.easternct.edu>
2019-04-18 18:00:37 -0400

    add basic code for functionality of gaplotter

--- plot/gaplot.py
+++ plot/gaplot.py
@@ -7,3 +7,13 @@ class GAPlotter:
     def __init__(self, datapath):
         self.datapath = datapath
         return
+
+"""
+os.chdir('trials')
+sns.set(color_codes=True)
+os.chdir('trials')
+ran = pd.read_csv('random_agent.csv')
+print(ran.keys())
+y = pd.DataFrame.hist(data=ran, column='Attempts')
+plt.show()
+"""
